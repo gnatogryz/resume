ac5d7255db66e2d9f2d11610b4a1d7595435942a
Michał Dobrzański <plague@go2.pl>
2022-02-22 19:46:39 +0100

    mugshot added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,15 @@ import './Header.scss';
 export default React.memo(function ({ children, subheader }: { children: ReactNode; subheader: string }) {
 	return (
 		<div id="main-header">
-			<h1>{children}</h1>
-			<span>{subheader}</span>
+			<div className="header-container">
+				<div className="left">
+					<img className="avatar" src="avatar.jpg" />
+				</div>
+				<div className="right">
+					<h1>{children}</h1>
+					<span>{subheader}</span>
+				</div>
+			</div>
 		</div>
 	);
 });
