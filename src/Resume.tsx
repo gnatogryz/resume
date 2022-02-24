0a4cdf01039e30e2461e46da3b23a296da1dd5a3
Michał Dobrzański <plague@go2.pl>
2022-02-24 13:06:18 +0100

    :pencil:

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Resume.scss';
 import Divider from './components/Divider';
 import Header from './components/Header';
